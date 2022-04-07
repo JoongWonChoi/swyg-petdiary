961f2be789dd3dac9fe52b356770a51ae6ecc048
JoongWonChoi <qweasd1130@naver.com>
2022-04-07 19:09:01 +0900

    Create project

---
+++ src/main/java/com/swyg/petdiary/PetdiaryApplication.java
@@ -0,0 +1,13 @@
+package com.swyg.petdiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetdiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetdiaryApplication.class, args);
+	}
+
+}
