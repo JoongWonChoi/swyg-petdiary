52aedd57e1dbae467439d0e21b4473dd065d6368
JoongWonChoi <qweasd1130@naver.com>
2022-04-18 23:22:56 +0900

    update

--- src/main/java/com/swyg/petdiary/dto/viewAllBoards/ViewAllBoardDto.java
+++
@@ -1,6 +0,0 @@
-package com.swyg.petdiary.dto.viewAllBoards;
-
-public class ViewAllBoardDto {
-    private Long id;
-    private String boardName;
-}
--- src/main/java/com/swyg/petdiary/dto/viewAllBoards/ViewAllBoardInterface.java
+++ src/main/java/com/swyg/petdiary/dto/viewAllBoards/ViewAllBoardInterface.java
@@ -1,6 +1,7 @@
 package com.swyg.petdiary.dto.viewAllBoards;
 
 public interface ViewAllBoardInterface {
+    /*select id, boardName . . 쿼리를 위한 용도*/
     Long getId();
     String getBoardName();
 }
